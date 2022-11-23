c6a333fc6f06bafcdc701732d63f466e1d0b9a84
0x000000000019b6 <0x000000000019b6@gmail.com>
2022-11-23 20:28:02 +0100

    .

--- Program.cs
+++ Program.cs
@@ -6,6 +6,9 @@ config.StartUrl = "local://app/index.html";
 //config.StartUrl = "local://app/index_frameless.html";
 //config.WindowOptions.WindowFrameless = true;
 
+config.CommandLineArgs.Add("no-sandbox","1");
+config.CommandLineArgs.Add("disable-software-rasterizer","1");
+config.CommandLineArgs.Add("disable-features", "DefaultPassthroughCommandDecoder");
 
 // ThreadApt.STA();
 
